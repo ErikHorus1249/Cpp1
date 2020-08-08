1a6f5b5569d31cdc2dd36329733370fc97276cbf
ErikHorus <tuananh1421999@gmail.com>
2020-08-08 16:29:33 +0700

    up

--- summer2020/bai7.cpp
+++ summer2020/bai7.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-int n, k, a[100];
-int stop = 0;
+int n, k, check, a[100];
+
 
 void init(){
   cin >> n;
@@ -10,38 +11,35 @@ void init(){
 }
 
 void res(){
-  for(int i = 1; i < k; i++){
-    if(i < k) cout << "(";
+  for(int i = 1; i <= k; i++){
+    if(i==1) cout << "(";
     cout << a[i];
     if(i < k) cout << " ";
-    else cout << ")";
+    else cout << ") ";
   }
 }
 
 void sinh(){
   int i = k;
-  while(i>0 && a[i]==1) i--;
-  if(i <= 0) stop = 1;
+  // Tim den phan tu khong phai la 1
+  while(i>0 && a[i] == 1) i--;
+  if(i<=0) check = 0; // cau hinh cuoi cung
   else{
-    a[i] -= 1;
-    int d = k - i +1;
+    a[i] --;
+    int d = k - i +1; 
     k = i;
-    for(int j = i+1; j <= i+d/a[i]; i++)
-      a[j] = a[i];
+    for(int j = i+1; j <= i+d/a[i]; j++) a[j] = a[i];
     k += d/a[i];
-    if(d%a[i]){
-      a[++k] = d%a[i];
-    }
+    if(d%a[i]) a[++k] = d%a[i];
   }
 }
 int main(){
   int t;
   cin >> t;
   while(t--){
-    cin >> n;
-    int stop = 0;
+    check = 1;
     init();
-    while(!stop){
+    while(check){
       res();
       sinh();
     }
